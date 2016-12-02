3f091f29b6c43c299720669e8410dc59e6f31c6c
Gábor Csárdi <csardi.gabor@gmail.com>
2016-12-02 22:03:32 +0000

    Linux via notify-send

--- R/linux.R
+++ R/linux.R
@@ -1,4 +1,15 @@
 
 notify_linux <- function(msg) {
-  stop("Linux not implemented yet")
+
+  ns <- Sys.which("notify-send")
+
+  if (ns == "") {
+    stop("Cannot find notify-send executable, you need to install it.\n",
+         "You need the 'libnotify-bin' package on Debian/Ubuntu, or\n",
+         "the 'libnotify' package on Fedora Linux.")
+  }
+
+  system2(ns, shQuote(c(" ", msg)))
+
+  invisible()
 }
