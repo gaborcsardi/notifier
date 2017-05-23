2caaa926c49e57961b59420a9f4b39be3f82e420
Gábor Csárdi <csardi.gabor@gmail.com>
2017-05-23 11:30:17 -0700

    Windows: run notifu in the background

--- R/windows.R
+++ R/windows.R
@@ -10,7 +10,9 @@ notify_windows <- function(msg, title, image) {
   }
 }
 
-#' @importFrom processx run
+bg_procs <- new.env(parent = emptyenv())
+
+#' @importFrom processx process
 
 notify_notifu <- function(msg, title, image) {
 
@@ -25,12 +27,29 @@ notify_notifu <- function(msg, title, image) {
     image <- normalizePath(system.file(package = packageName(), "R.ico"))
   }
 
-  ## TODO: in the background
+  ## Unfortunately notifu takes a while to finish, so it is a bit harder
+  ## to clean it up, because we need to run it in the background.
+  ## We store the process objects in a list, within an environment in the
+  ## package. Every time a new process is added we also check for process
+  ## objects that have been in the list for a while, and remove them.
+
   args <- c("/m", msg, "/p", title, "/i", image)
-  run(notifu, args, timeout = 2)
+  p <- process$new(notifu, args)
+
+  now <- Sys.time()
+  bg_procs$p <- c(bg_procs$p, list(list(proc = p, ts = now)))
+
+  timeout <- as.difftime(5, units = "secs")
+  rm <- Filter(
+    function(i) now - bg_procs$p[[i]]$ts > timeout,
+    seq_along(bg_procs$p)
+  )
+  if (length(rm)) bg_procs$p <- bg_procs$p[-rm]
 
   invisible()
 }
+
+#' @importFrom processx run
 
 notify_toaster <- function(msg, title, image) {
 
